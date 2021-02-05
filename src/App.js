65d27d829901d95f884438bef6602c42fdc08856
Ivan Sydorenko1 <Ivan_Sydorenko1@epam.com>
2021-02-05 18:33:40 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useRef, useState} from "react";
 
-function App() {
+const TIME_ZONE_URL = 'http://api.timezonedb.com/v2.1/list-time-zone?key=XWSLLPX5RMIZ&format=json&zone=Europe/*';
+const TIME_URL = 'http://api.timezonedb.com/v2/get-time-zone?key=XWSLLPX5RMIZ&format=json&by=zone&zone=';
+
+const App = () => {
+  const [timeZonesList, setTimeZonesList] = useState([]);
+  const [timeZone, setTimeZone] = useState('');
+  const [time, setTime] = useState('');
+  const intervalId = useRef(0);
+
+  const getData = async (url) => {
+    try {
+      const response = await fetch(url);
+      return await response.json();
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  };
+
+  const updateTimeZones = async () => {
+    const data = await getData(TIME_ZONE_URL) || {zones: []};
+    setTimeZonesList(data.zones);
+
+    // choose the first timezone by default
+    setTimeZone(data.zones[0].zoneName);
+  };
+
+  const updateTime = async () => {
+    const data = await getData(`${TIME_URL}${timeZone}`) || {formatted: []};
+    setTime(data.formatted);
+  };
+
+  const updateTimeString = () => {
+    if (intervalId.current) {
+      clearInterval(intervalId.current);
+    }
+
+    updateTime();
+
+    intervalId.current = setInterval(updateTime, 5000);
+  };
+
+  useEffect(() => {
+    updateTimeZones();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (timeZone && !time) {
+      updateTimeString();
+    }
+  }, [time, timeZone, updateTime, updateTimeString]);
+
+  const handleChange = event => {
+    setTimeZone(event.target.value);
+    updateTimeString();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <label>
+          Please choose timezone:
+          <div>
+            <select value={timeZone} onChange={handleChange}>
+              {timeZonesList.map(({zoneName}) => <option key={zoneName} value={zoneName}>{zoneName}</option>)}
+            </select>
+          </div>
+          <div>{time}</div>
+        </label>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders label', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+
+  const linkElement = screen.getByText(/Please choose timezone:/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// TODO add more tests
+// sorry, don't have enough time for this
